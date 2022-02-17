ccd1be3606086436f450394256cf080fcf092c8a
unknown <vinhhoangngoc22@gmail.com>
2022-02-17 22:44:01 +0700

    first commit

---
+++ src/main/java/com/javatechie/querydsl/controller/AuthBookController.java
@@ -0,0 +1,57 @@
+package com.javatechie.querydsl.controller;
+
+import com.javatechie.querydsl.dto.AuthorStatistic;
+import com.javatechie.querydsl.entity.Author;
+import com.javatechie.querydsl.entity.Book;
+import com.javatechie.querydsl.service.AuthBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/rest")
+public class AuthBookController {
+
+    @Autowired
+    private AuthBookService authBookService;
+
+
+    @PostMapping("/authors/book")
+    public List<Author> saveAuthorsWithBooks(@RequestBody List<Author> authors) {
+        return authBookService.saveAuthorsWithBooks(authors);
+    }
+
+    @GetMapping("/authors")
+    public List<Author> getAllAuthors() {
+        return authBookService.getAuthors();
+    }
+
+    @GetMapping("/books")
+    public List<Book> getALlBooks() {
+        return authBookService.getBooks();
+    }
+
+    @GetMapping("/author/{email}")
+    public Optional<Author> findAuthorByEmail(@PathVariable(value= "email") String email) {
+        return authBookService.findAuthorByEmail(email);
+    }
+    
+    @GetMapping("/name")
+    public Optional<Author> findAuthorByName(@RequestParam("name") String name) {
+    	System.out.println(name);
+        return authBookService.findAuthorByName(name);
+    }
+
+    @GetMapping("/authorStatistic")
+    public List<AuthorStatistic> getAuthorStatistic() {
+        return authBookService.getAuthorStatistic();
+    }
+
+    //to avoid N+1 problem in hibernate/jpa
+    @GetMapping("/authors/fetchJoin")
+    public List<Author> getAuthorsWithFetchJoin() {
+        return authBookService.getAuthorsWithFetchJoin();
+    }
+}
